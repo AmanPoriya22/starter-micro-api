42d7bfbd5a0fdc3a42cb9bcb3c606c12a37113e1
Poriya Aman <92879606+AmanPoriya22@users.noreply.github.com>
2023-04-15 00:01:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const functions = require('firebase-functions');
+const agora = require('agora-access-token');
+
+exports.generateToken = functions.https.onCall(async (data, context) => {
+  const appId = 'f8001179bb744831b3f0099a81e5b0cb';
+  const appCertificate = '649815119f6b43f9851acaaa7fe7f23e';
+  const uid = data.uid; // The unique identifier of the user requesting the token
+  const role = agora.RtcRole.PUBLISHER; // The role of the user requesting the token
+  const expirationTimeInSeconds = 3600; // The expiration time of the token in seconds
+
+  const channelName = generateChannelName(); // Generate a unique channel name
+
+  const token = agora.generateToken(appId, appCertificate, channelName, uid, role, expirationTimeInSeconds);
+
+  return {"token": token, "channelName": channelName};
+});
+
+function generateChannelName() {
+  // Generate a random channel name using a timestamp
+  return 'channel-' + Date.now().toString();
+}
